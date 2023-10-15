d99669ca38dafba5adc541733820b809557e8202
Daniel Connelly <dhconnelly@gmail.com>
2023-10-15 16:14:22 +0200

    use gmock

--- distlog/test.cc
+++ distlog/test.cc
@@ -1,6 +1,14 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "distlog/greeting.pb.h"
 
+using distlog::Greeting;
+using testing::Property;
+
 TEST(Foo, Bar) {
-    ASSERT_TRUE(true);
+    auto greeting = Greeting::default_instance();
+    greeting.set_greeter("me");
+    greeting.set_greeted("you");
+    greeting.set_greeting("hello, world");
+    EXPECT_THAT(greeting, Property(&Greeting::greeting, "hello, world"));
 }
